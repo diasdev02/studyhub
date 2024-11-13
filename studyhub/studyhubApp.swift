f0f1498d298026b5f046cf34ed9dfb359f893968
diasdev02 <diasdev@icloud.com>
2024-11-13 16:50:15 +0500

    Initial Commit

---
+++ studyhub/studyhubApp.swift
@@ -0,0 +1,20 @@
+//
+//  studyhubApp.swift
+//  studyhub
+//
+//  Created by Dias Akyshev on 13.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct studyhubApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
